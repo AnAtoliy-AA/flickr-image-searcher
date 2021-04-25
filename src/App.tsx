7f69074b8eca75ac13048dac4df1e261763aa32a
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-04-25 19:50:42 +0300

    fix: remove unnecessary logs

--- src/App.tsx
+++ src/App.tsx
@@ -12,27 +12,20 @@ import IdleMask from "./components/IdleMask/IdleMask";
 function App() {
   const [isIdle, setIsIdle] = useState(DEFAULT_VALUES.FALSE);
   
-  const handleOnIdle = (event: any) => {
-    // console.log('user is idle', event)
-    console.log('last active', getLastActiveTime())
+  const handleOnIdle = () => {
+    //TODO Last user usage
+    // console.log('last active', getLastActiveTime())
     setIsIdle(DEFAULT_VALUES.TRUE);
   }
 
-  const handleOnActive = (event: any) => {
-    // console.log('user is active', event)
-    console.log('time remaining', getRemainingTime())
+  const handleOnActive = () => {
     setIsIdle(DEFAULT_VALUES.FALSE);
   }
 
-  //TODO maybe not need it
-  // const handleOnAction = (event: any) => {
-  //   console.log('user did something', event)
-  // }
-  const { getRemainingTime, getLastActiveTime } = useIdleTimer({
+  useIdleTimer({
     timeout: DEFAULT_VALUES.IDLE_DELAY_TIME,
     onIdle: handleOnIdle,
     onActive: handleOnActive,
-    // onAction: handleOnAction,
     debounce: DEFAULT_VALUES.IDLE_DEBOUNCE,
   })
 
